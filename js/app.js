7bca2aa7f4438e18b4eea7a2b98e6d25354643fb
Isratisu19 <115801624+Isratisu19@users.noreply.github.com>
2023-01-05 10:51:27 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.slickSlider').slick({
+        slidesToShow: 6,
+        dots: true,
+        centerMode: true,
+        centerPadding: '0px',
+        autoplay: true,
+        autoplaySpeed: 500,
+        infinite: false,
+
+    });
+    
+})
+
+
